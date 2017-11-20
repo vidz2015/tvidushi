5c5313ec76fd8925fe5a21ec817ff86df60a4dd8
vidushi <takshila.vidushi@gmail.com>
2017-11-20 17:20:54 +0530

    commiting rest client

--- spring-rest-example/src/test/java/com/tvidushi/TestSpringRestExample.java
+++ spring-rest-example/src/test/java/com/tvidushi/TestSpringRestExample.java
@@ -1,19 +1,31 @@
 package com.tvidushi;
 
+import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 
 import org.springframework.web.client.RestTemplate;
 
 import com.tvidushi.model.Student;
 
+
+
 public class TestSpringRestExample {
 	
 	
 	public static final String URI = "http://localhost:8080/spring-rest-example";
+	public static final String DUMMY_STUD = URI+"/rest/stud/dummy";
+	public static final String GET_STUDENT = URI+"/rest/stud/{id}";
+	public static final String GET_ALL = URI+"/rest/studs";
+	public static final String CREATE = URI+"/rest/stud/create";
+	public static final String DELETE = URI+"/rest/stud/delete/1";
 	
 	public void main(String args[]){
 		
 		testCreateStudent();
+		testGetAllStudents();
+		testGetEmployee();
+
 	}
 
 	
@@ -22,31 +34,28 @@ public class TestSpringRestExample {
 		Student student = new Student();
 		student.setId(1);
 		student.setName("vidushi");
-		Student res = restTemplate.postForObject(URI+"/rest/stud/create", student, Student.class);  //uri,the object that is to passed, & returntype object expected
+		Student res = restTemplate.postForObject(CREATE, student, Student.class);  //uri,the object that is to passed, & returntype object expected
 		
+		System.out.println("ID = "+res.getId()+",Name = "+res.getName());
+	}
+	
+	
+	public static void testGetEmployee(){
+		RestTemplate restTemplate = new RestTemplate();
+		Student res = restTemplate.getForObject(GET_STUDENT,Student.class);
 		System.out.println("ID = "+res.getId()+",Name = "+res.getName());
 	}
 	
 	
 	public static void testGetAllStudents(){
 		RestTemplate restTemplate = new RestTemplate();
-		
-		LinkedHashMap<Student> hashMap = restTemplate.getForObject(url, responseType, urlVariables)
-		
-		
-		
+		List<Student> studentlist =(List) restTemplate.getForObject(GET_ALL, List.class);
+		studentlist.forEach(student->{
+			System.out.println("Id is :"+student.getId()+" "+student.getName());
+		});
 	}
 	
 
-	
-	public static void testGetEmployee(){
-		RestTemplate restTemplate = new RestTemplate();
-	}
-	
-	public static void testGetDummyEmployee(){
-		RestTemplate restTemplate = new RestTemplate();
-	}
-	
 	
 
 }
